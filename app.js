84ff868c1768d90b58a393cf28c71d03038c6053
Masum Billah <masum.monpura@gmail.com>
2022-09-06 21:00:18 +0600

    done till 2

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('hello')
+//task: 1
+console.log(1);
+console.log(2);
+setTimeout(() => {
+  console.log(3);
+}, 3500);
+console.log(4);
+console.log(5);
+
+//task: 2
+
+const num = prompt("please give us a number");
+
+const result = alert(
+  `your number is added with 200 and the result is ${parseInt(num) + 200}`
+);
